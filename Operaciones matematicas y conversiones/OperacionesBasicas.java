808ec6944cee552511be02ca40ee216bcf0be267
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2025-06-02 20:11:28 -0600

    Create  OperacionesBasicas.java

---
+++ Operaciones matematicas y conversiones/OperacionesBasicas.java
@@ -0,0 +1,16 @@
+
+package operacionesbasicas;
+
+public class OperacionesBasicas {
+    public static void main(String[] args) {
+        //Declaración
+        int a = 10, b = 3;
+        
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+        System.out.println("Módulo: " + (a % b));
+    }
+    
+}
