60f7e85d792294e770a5922daea31281f4ddf716
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2025-06-02 20:20:54 -0600

    Create areaCirculo.java

---
+++ Operaciones matematicas y conversiones/areaCirculo.java
@@ -0,0 +1,13 @@
+
+package areacirculo;
+
+public class AreaCirculo {
+    public static void main(String[] args) {
+        //Declaración
+        double radio = 2;
+        
+        double area = Math.PI * Math.pow(radio, 2);
+        //Imprimir Area
+        System.out.println(area);
+    }
+}
