cac56e34dc86e052987c798f0b03eb16a9537fbf
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2025-06-02 20:36:28 -0600

    Create celsiusFarenheit.java

---
+++ Operaciones matematicas y conversiones/celsiusFarenheit.java
@@ -0,0 +1,14 @@
+
+package celsiusfarenheit;
+
+public class CelsiusFarenheit {
+    public static void main(String[] args) {
+        //Declaración
+        double celsius = 10.1;
+        
+        double farenheit = (celsius * (9/5))+32;
+        
+        System.out.println("La temperatura en celsius es: "+celsius);
+        System.out.println("La temperatura en farenheit es: "+farenheit);
+    }
+}
